6aa76ef3480a412e6ea673c320c07b69190920ca
Ian <mus5002@aol.com>
2020-03-26 20:00:59 -0400

    added test for returning 200 code and all todos from TodoController.getTodos

--- routes/todo_routes.js
+++ routes/todo_routes.js
@@ -3,5 +3,6 @@ const todoController = require('../controllers/todo_controller')
 const router = express.Router()
 
 router.post('/', todoController.createTodo)
+router.get('/', todoController.getTodos)
 
 module.exports = router
--- tests/unit/todo_controller.test.js
+++ tests/unit/todo_controller.test.js
@@ -2,14 +2,36 @@ const TodoController = require('../../controllers/todo_controller')
 const TodoModel = require('../../models/todo_model')
 const httpMocks = require('node-mocks-http')
 const newTodo = require('../mock-data/new-todo.json')
+const allTodos = require('../mock-data/all-todos.json')
 
 TodoModel.create = jest.fn()
+TodoModel.find = jest.fn()
 
 let req, res, next;
 beforeEach(() => {
     req = httpMocks.createRequest()
     res = httpMocks.createResponse()
     next = jest.fn()
+})
+
+describe('todo controller get todos', () => {
+    it('should have a get todos function', () => {
+        expect(typeof TodoController.getTodos).toBe('function')
+    })
+
+    it('should call TodoModel.find({})', async () => {
+        await TodoController.getTodos(req, res, next)
+        expect(TodoModel.find).toHaveBeenCalledWith({})
+    })
+
+    it('should return 200 response code and all todos', async () => {
+        TodoModel.find.mockReturnValue(allTodos)
+        await TodoController.getTodos(req, res, next)
+
+        expect(res.statusCode).toBe(200)
+        expect(res._isEndCalled()).toBeTruthy()
+        expect(res._getJSONData()).toStrictEqual(allTodos)
+    })
 })
 
 describe('todo controller creates todo', () => {
